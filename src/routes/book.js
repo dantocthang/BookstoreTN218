9409485b51058b3381d2063f5048edc7c36bd28a
hntdienit <hntdienit@gmail.com>
2022-10-24 01:04:26 +0700

    mailCheck-booKDetail

--- src/routes/book.js
+++ src/routes/book.js
@@ -5,5 +5,6 @@ import BookController from '../app/controllers/BookController.js'
 const router = express.Router();
 
 router.get('/', BookController.getBooksList);
+router.get('/:bookId', BookController.getBookDetail);
 
 export default router;
